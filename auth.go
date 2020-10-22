5f88942861b3803060f1723c48d00aa0358af1a3
Dmitry Paramonov <gamiysikdima@gmail.com>
2020-10-22 21:09:25 +0300

    auth methods

--- auth.go
+++ auth.go
@@ -3,6 +3,8 @@ package achieve_models_go
 const (
 	AUTH_REGISTER_KEY = "register"
 	AUTH_ISREGISTERED_KEY = "isreg"
+	AUTH_IS_USER_REGISTERED_KEY = "isreg_u"
+	AUTH_IS_EMAIL_REGISTERED_KEY = "isreg_e"
 	AUTH_AUTHENTICATE_KEY = "authenticate"
 	AUTH_AUTHORIZE_KEY = "authorize"
 )
@@ -18,6 +20,30 @@ type RegisterResponse struct {
 	Request
 	ResponseCode int
 	Error        string
+}
+
+type IsEmailRegisteredRequest struct {
+	Request
+	Email    string
+}
+
+type IsEmailResponse struct {
+	Request
+	ResponseCode         int
+	IsEmailRegistered    bool
+	Error                string
+}
+
+type IsUserRegisteredRequest struct {
+	Request
+	Username string
+}
+
+type IsUserRegisteredResponse struct {
+	Request
+	ResponseCode         int
+	IsUsernameRegistered bool
+	Error                string
 }
 
 type IsRegisteredRequest struct {
